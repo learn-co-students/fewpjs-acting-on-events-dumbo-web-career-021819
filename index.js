741d3279fbe8c8867b53b65795f2b9ba95787531
James Dorr <jamesdorr3@gmail.com>
2019-04-02 18:25:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+    else if (e.key === 'ArrowRight') {
+        moveDodgerRight()
+    }
+})
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger')
+    const styleLeft = dodger.style.left
+    const stringNumber = styleLeft.replace("px","")
+    const left = parseInt(stringNumber, 10)
+    dodger.style.left = `${left - 1}px`
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger')
+    const styleLeft = dodger.style.left
+    const stringNumber = styleLeft.replace("px","")
+    const left = parseInt(stringNumber, 10)
+    dodger.style.left = `${left + 1}px`
+}
