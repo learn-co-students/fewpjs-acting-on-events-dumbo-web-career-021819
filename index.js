0c8963032b7ade4b2e3f6a28f232100450e6bdc1
campbelllssoup <matjocampbell@gmail.com>
2019-04-02 14:22:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+let dodger = document.querySelector('#dodger');
+
+function moveDodgerLeft() {
+  let left = dodger.style.left;
+  let left_int = parseInt(left);
+  left_int -= 10;
+  dodger.style.left = `${left_int}px`;
+};
+
+function moveDodgerRight() {
+  let left = dodger.style.left;
+  let left_int = parseInt(left);
+  left_int += 10;
+  dodger.style.left = `${left_int}px`;
+};
